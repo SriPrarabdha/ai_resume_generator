9943fbd4f06136466844e01adb1d76da67de605e
Prarabdha Srivastava <sriprarabdha2004@gmail.com>
2024-09-15 15:32:42 +0530

    added blob token config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-     BLOB_READ_WRITE_TOKEN: process.env.BLOB_READ_WRITE_TOKEN,
+      BLOB_READ_WRITE_TOKEN: process.env.BLOB_READ_WRITE_TOKEN,
     },
     images: {
       remotePatterns: [
